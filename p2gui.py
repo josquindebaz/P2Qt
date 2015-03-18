37542af22a0198481b9258a2b50ad0ed03fe0957
Josquin Debaz <josquin.debaz@gmail.com>
2015-03-18 14:20:56 +0100

    modified:   p2gui.py

--- p2gui.py
+++ p2gui.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-#TODO internationalisation QTranslator
 
 import sys
 import PySide
@@ -79,8 +78,6 @@ class client(object):
 	def eval_set_ctx(self, sem_txt, field, val):
 		return self.c.eval_set_ctx(sem_txt, field, val)
 
-	def creer_msg_set_ctx(self,data):
-		return self.c.creer_msg_set_ctx(data)
 
 
 class Principal(QtGui.QMainWindow):
@@ -114,8 +111,6 @@ class Principal(QtGui.QMainWindow):
 		nbre_txt = len (listeTextes)
 		# mise en cache  valeur - semtxt
 		for text in listeTextes :
-
-
 			sem = u"$txt%s"%indice
 			txt_name = listeTextes[indice]
 			cle = txt_name + u"$txt"
@@ -1055,6 +1050,7 @@ class Principal(QtGui.QMainWindow):
 		elif ( self.SubWdwSETabs.currentIndex () == 2 ):
 			self.show_textContent( sem_txt)
 
+		#selectionne le texte dans l'onglet corpus
 		self.CorpusTexts.setCurrentRow(self.liste_semtxt_ord.index(self.semantique_txt_item))
 		#TODO selectionner les memes textes selectionnes dans les listes differentes
 		if (self.SOT1.count() > 1):
@@ -1113,22 +1109,20 @@ class Principal(QtGui.QMainWindow):
 		for r in range( self.textCTX.rowCount()):
 			field = self.textCTX.item(r,0).text()
 			val =  self.textCTX.item(r,1).text()
-			ask = u"%s.%s" % ( self.m_current_selected_semtext,field)
+			ask = u"%s.ctx.%s" % ( self.m_current_selected_semtext,field)
 			self.client.eval_var(ask)
 			result = re.sub(u"^\s*","",self.client.eval_var_result)
-#TODO ne met pas à jour le CTX, a un pb avec result
 			if (result != val):
 				print [field, result, val]
 				self.client.eval_set_ctx( self.m_current_selected_semtext,field,val)
-		
-		#self.client.creer_msg_set_ctx ( (sem_txt, field, val) )
-		#self.client.eval_set_ctx(sem_txt, field, val)
-		#print (sem_txt, field, val)
+				self.client.eval_set_ctx( self.m_current_selected_semtext,"test",val)
 		
 		self.textCTX_valid.setEnabled(False)
 		self.textCTX_reset.setEnabled(False)
+#TODO ne met pas à jour le CTX, 
 #TODO verifier que le cache soit mis à jour
-		self.show_textCTX(self.m_current_selected_semtext)
+		#self.show_textCTX(self.m_current_selected_semtext)
+		self.resetCTX()
 
 
 		
